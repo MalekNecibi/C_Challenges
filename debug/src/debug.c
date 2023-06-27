a5ff31a7758e0c0e97f2846bdf900f42bb344c94
Malek N <Malek@Necibi.us>
2023-06-27 13:57:37 -0500

    debug v0.1 tests pass commented

--- debug/src/debug.c
+++ debug/src/debug.c
@@ -6,25 +6,44 @@
 
 // protected function, that only this .c can use
 int comparator_func(const void *a, const void *b) {
-    return (uint8_t)a - (uint8_t *)b;
+    return *(uint16_t*)a - *(uint16_t*)b;
 }
 
 bool terrible_sort(uint16_t *data_array, const size_t value_count) {
-    uint16_t sorting_array *= malloc(value_count * sizeof(*data_array));
-    for (int i = 0; i < value_count; ++i) {
+    if (!data_array || 0 == value_count) {
+        return false;
+    }
+    
+    size_t num_bytes = value_count * sizeof(uint16_t);
+    
+    // Clone dataset for sort, prevent corruption
+    uint16_t* sorting_array = malloc(num_bytes);
+    if (!sorting_array) {
+        return false;
+    }
+    
+    // Copy to the Clone
+    // keeping manual comparison bc spirit of this challenge
+    // memcpy(sorting_array, data_array, num_bytes);
+    for (size_t i = 0; i < value_count; i++) {
         sorting_array[i] = data_array[i];
     }
+    
+    // Perform sort
+    qsort(sorting_array, value_count, sizeof(uint16_t), comparator_func);
 
-    qsort(sorting_array, value_count, sizeof(sorting_array) / sizeof(uint16_t), comparator_func)
-
+    // Test if sort succeeded
     bool sorted = true;
-    for (int i = 0; i < value_count ++i) {
-        sorted &= sorting_array[i] < sorting_array[i + 1];
+    for (size_t i = 0; i < value_count-1; i++) {
+        sorted = sorted && (sorting_array[i] <= sorting_array[i + 1]);
+    }
+    
+    // commit the changes
+    if (sorted) {
+        memcpy(data_array, sorting_array, num_bytes);
     }
 
-    if (sorted) {
-        memcpy(sorting_array, *data_array, value_count);
-    }
+    free(sorting_array);
     return sorted;
 }
 
