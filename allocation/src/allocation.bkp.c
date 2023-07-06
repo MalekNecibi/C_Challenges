8e0bec8763532c01a5da88f2fda8c5e7db7e736c
Malek N <Malek@Necibi.us>
2023-07-06 14:43:41 -0500

    add allocation

---
+++ allocation/src/allocation.bkp.c
@@ -0,0 +1,24 @@
+#include "../include/allocation.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+
+void* allocate_array(size_t member_size, size_t nmember,bool clear)
+{
+
+}
+
+void* reallocate_array(void* ptr, size_t size)
+{
+
+}
+
+void deallocate_array(void** ptr)
+{
+
+}
+
+char* read_line_to_buffer(char* filename)
+{
+
+}
